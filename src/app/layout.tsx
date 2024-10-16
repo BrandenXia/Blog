c30c04218ada1c3403129d57964443a2396a9619
BrandenXia <xxtbranden@outlook.com>
2024-10-16 07:40:35 -0700

    feat: add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
-import "./globals.css";
-
 import { Inter, JetBrains_Mono } from "next/font/google";
 
 import Providers from "@/components/providers";
 import { JSXWrapper } from "@/types/jsx-wrapper";
 import config from "@data/config";
+import { Analytics } from "@vercel/analytics/next";
+
+import "./globals.css";
 
 const { metadata } = config;
 
@@ -24,6 +25,7 @@ const Layout: JSXWrapper = ({ children }) => (
   <html lang="en">
     <body className={`${inter.variable} ${jetbrainsMono.variable} font-sans antialiased`}>
       <Providers>{children}</Providers>
+      <Analytics />
     </body>
   </html>
 );
