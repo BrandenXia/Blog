07d8595e79ff9a18c1f574ddcbf22c76f18cca5e
BrandenXia <xxtbranden@outlook.com>
2024-10-16 07:53:11 -0700

    feat: posts are included in sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,18 +1,30 @@
 import { MetadataRoute } from "next";
 
-const sitemap = (): MetadataRoute.Sitemap => [
-  {
-    url: "/",
-    lastModified: new Date(),
-    changeFrequency: "monthly",
-    priority: 1,
-  },
-  {
-    url: "/posts",
-    lastModified: new Date(),
-    changeFrequency: "weekly",
-    priority: 0.8,
-  },
-];
+import { getPostsBy } from "@/lib/posts";
+
+const sitemap = async (): Promise<MetadataRoute.Sitemap> => {
+  const posts = await getPostsBy({ limit: "all" });
+
+  return [
+    {
+      url: "/",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+    {
+      url: "/posts",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    ...posts.map((post) => ({
+      url: `/posts/${post.slug}`,
+      lastModified: post.metadata.date,
+      changeFrequency: "weekly" as const,
+      priority: 0.7,
+    })),
+  ];
+};
 
 export default sitemap;
